ce8070186f1b6018ea773e22e3c08c347008e12e
mamun <kabir.chowdhury1990@gmail.com>
2024-02-28 02:42:00 +0530

    refresh controller modified

--- lib/features/home/controller/refresh_controller.dart
+++ lib/features/home/controller/refresh_controller.dart
@@ -2,10 +2,11 @@ import 'package:get/get.dart';
 import 'package:janatar_barta/features/home/controller/news_controller.dart';
 
 class RefreshController extends GetxController {
-
   NewsController controller = Get.put(NewsController());
   Future<void> handleRefresh() async {
-    controller.getNews();
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(
+      const Duration(seconds: 3),
+      () => controller.getNews(),
+    );
   }
 }
